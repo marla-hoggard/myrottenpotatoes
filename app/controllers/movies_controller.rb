c202022373e47423614e58861bc4ef8eceb028ff
Marla Glasser <mbglasser@gmail.com>
2018-05-02 17:45:33 -0400

    Implement standard CRUD actions and corresponding views for movies.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,37 @@ class MoviesController < ApplicationController
 		@movie = Movie.find(params[:id])
 	end
 
+	def new
+		@movie = Movie.new
+	end
+
+	def create
+		params.require(:movie)
+		permitted = params[:movie].permit(:title, :rating, :release_date)
+		@movie = Movie.create!(permitted)
+		flash[:notice] = "'#{@movie.title}'' was successfully created."
+		redirect_to movies_path
+	end
+
+	def edit
+		@movie = Movie.find(params[:id])
+	end
+
+	def update
+		params.require(:movie)
+		permitted = params[:movie].permit(:title, :rating, :release_date)
+
+		@movie = Movie.find(params[:id])
+		@movie.update_attributes!(permitted)
+		flash[:notice] = "'#{@movie.title}'' was successfully updated."
+		redirect_to movie_path(@movie)
+	end
+
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+		flash[:notice] = "'#{@movie.title}'' was successfully deleted."
+		redirect_to movies_path
+	end
+
 end
